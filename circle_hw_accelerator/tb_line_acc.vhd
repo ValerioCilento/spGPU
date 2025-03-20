ee02294152dc9e46a428caf6942493ff08ccf074
valerioh_ <valecilento02@gmail.com>
2025-03-20 10:58:33 +0100

    --

--- circle_hw_accelerator/tb_line_acc.vhd
+++ circle_hw_accelerator/tb_line_acc.vhd
@@ -68,7 +68,7 @@ rst <= '1', '0' after 20 ns;
 start <= '1', '0' after 30 ns;
 xc <= std_logic_vector(to_unsigned(100, 8));
 yc <= std_logic_vector(to_unsigned(100, 8));
-r <= std_logic_vector(to_unsigned(100, 8));
+r <= std_logic_vector(to_unsigned(20, 8));
 
 color<= x"FF0000";
 end Behavioral;
