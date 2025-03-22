66b7c2ac48318506ceb4c9f62816464f3e9506e2
Lucaf03 <luca@Ubuntu22.myguest.virtualbox.org>
2025-03-22 18:00:06 +0100

    Added Edge Equations' Hw

---
+++ fill_hw_accelerator/edge_fill.vhd
@@ -0,0 +1,109 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/22/2025 05:07:53 PM
+-- Design Name: 
+-- Module Name: edge_fill - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity edge_fill is
+Port ( 
+    x1, y1, x2, y2, px, py : in std_logic_vector(8 downto 0); --TODO: RIMETTERE GLI INPUT A 7 BIT MA UTILIZZARE SEGNALI A 8 IN CUI L'OTTAVO E' ZERO 
+    clk, rst, start : in std_logic;
+    done : out std_logic;
+    area : out std_logic_vector(17 downto 0)
+);
+end edge_fill;
+
+architecture Behavioral of edge_fill is
+    type state_t is (IDLE, CALC1, CALC2, CALC3, FINISH);
+    signal state, next_state : state_t;
+    signal reg : signed(17 downto 0);
+    signal tmp1, tmp2 : signed(8 downto 0);
+ 
+begin
+    process(all) begin 
+        done <= '0';
+        if rst = '1' then
+            next_state <= IDLE;
+        else 
+            case state is 
+                when IDLE => 
+                    if start = '1' then
+                        next_state <= CALC1;
+                    else 
+                        next_state <= IDLE;
+                    end if;
+                when CALC1 => 
+                    next_state <= CALC2;
+                when CALC2 =>
+                    next_state <= CALC3;
+                when CALC3 =>
+                    next_state <= FINISH;
+                when FINISH =>
+                    done <= '1';
+                    next_state <= IDLE;
+                when others => 
+                    next_state <= IDLE;
+             end case;
+        end if;
+    end process;
+    
+    process(clk, rst) begin
+        if rst = '1' then
+            state <= IDLE;
+        elsif rising_edge(clk) then
+            state <= next_state;
+        end if;
+    end process;
+    
+    data_path : process(clk, rst) 
+        variable tmp3 : signed(8 downto 0) := (others => '0');
+    begin
+        if rst = '1' then
+            reg <= (others => '0');
+            tmp1 <= (others => '0');
+            tmp2 <= (others => '0');
+        elsif rising_edge(clk) then
+            case state is 
+                when IDLE =>
+                    reg <= (others => '0');
+                when CALC1 =>
+                    tmp1 <= signed(px) - signed(x1);
+                    tmp2 <= signed(y2) - signed(y1);
+                when CALC2 =>
+                    reg <= tmp1*tmp2;
+                    tmp1 <= signed(py) - signed(y1);
+                    tmp2 <= signed(x2) - signed(x1);
+                when CALC3 =>
+                    area <= std_logic_vector(reg - (tmp1*tmp2));
+                when FINISH =>
+                    null;
+                when others =>
+                    null;
+            end case;
+        end if;
+    end process;
+end Behavioral;
---
+++ fill_hw_accelerator/tb_edge_fill.vhd
@@ -0,0 +1,81 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/22/2025 05:40:16 PM
+-- Design Name: 
+-- Module Name: tb_edge_fill - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity tb_edge_fill is
+--  Port ( );
+end tb_edge_fill;
+
+architecture Behavioral of tb_edge_fill is
+    component edge_fill is
+    Port ( 
+        x1, y1, x2, y2, px, py : in std_logic_vector(8 downto 0); 
+        clk, rst, start : in std_logic;
+        done : out std_logic;
+        area : out std_logic_vector(17 downto 0)
+    );
+    end component;
+    
+    signal x1, y1, x2, y2, px, py : std_logic_vector(8 downto 0);
+    signal clk, rst, start, done : std_logic;
+    signal area : std_logic_vector(17 downto 0);
+begin
+    U1 : edge_fill port map(
+        clk => clk,
+        rst => rst,
+        start => start,
+        done => done,
+        x1 => x1,
+        x2 => x2,
+        y1 => y1,
+        px => px,
+        py => py,
+        y2 => y2,
+        area => area
+    );
+    process begin
+        clk <= '1';
+        wait for 5 ns;
+        clk <= '0';
+        wait for 5 ns;
+    end process;
+
+rst <= '1', '0' after 20 ns;
+start <= '1', '0' after 40 ns;
+x1 <= std_logic_vector(to_unsigned(100, 9));
+y1 <= std_logic_vector(to_unsigned(100, 9));
+x2 <= std_logic_vector(to_unsigned(70, 9));
+y2 <= std_logic_vector(to_unsigned(240, 9));
+px <= std_logic_vector(to_unsigned(150, 9));
+py <= std_logic_vector(to_unsigned(150, 9));
+
+end Behavioral;
