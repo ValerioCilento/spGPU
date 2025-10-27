0d279c1b0c6069beeef92478c739389d5864b77d
Lucaf03 <lucafilogna18@gmail.com>
2025-10-27 18:57:07 +0100

    Update AXI STREAM

--- myaxistream_1_0/hdl/myaxistream_v1_0.vhd
+++ myaxistream_1_0/hdl/myaxistream_v1_0.vhd
@@ -4,12 +4,13 @@ use ieee.numeric_std.all;
 
 entity myaxistream_v1_0 is
 	generic (
-		C_S_AXIS_TDATA_WIDTH	: integer	:= 32
+		C_S_AXIS_TDATA_WIDTH	: integer	:= 64
 	);
 	port (
         ready_enb : in std_logic;
-        led : out std_logic;
-        data_o : out std_logic_vector(2 downto 0);
+        instr_valid : out std_logic;
+	    data_1_o : out std_logic_vector(31 downto 0);
+	    data_2_o : out std_logic_vector(31 downto 0);
 		s_axis_aclk	: in std_logic;
 		s_axis_aresetn	: in std_logic;
 		s_axis_tready	: out std_logic;
@@ -23,12 +24,13 @@ architecture arch_imp of myaxistream_v1_0 is
 	-- component declaration
 	component myaxistream_v1_0_S_AXIS is
 		generic (
-		C_S_AXIS_TDATA_WIDTH	: integer	:= 32
+		C_S_AXIS_TDATA_WIDTH	: integer	:= 64
 		);
 		port (
 		ready_enb : in std_logic;
-		led : out std_logic;
-		data_o : out std_logic_vector(2 downto 0);
+		instr_valid : out std_logic;
+	    data_1_o : out std_logic_vector(31 downto 0);
+	    data_2_o : out std_logic_vector(31 downto 0);
 		S_AXIS_ACLK	: in std_logic;
 		S_AXIS_ARESETN	: in std_logic;
 		S_AXIS_TREADY	: out std_logic;
@@ -45,13 +47,14 @@ myaxistream_v1_0_S_AXIS_inst : myaxistream_v1_0_S_AXIS
 		C_S_AXIS_TDATA_WIDTH	=> C_S_AXIS_TDATA_WIDTH
 	)
 	port map (
-	    data_o => data_o,
+        data_1_o => data_1_o,
+        data_2_o => data_2_o,
 		S_AXIS_ACLK	=> s_axis_aclk,
-		led => led,
 		S_AXIS_ARESETN	=> s_axis_aresetn,
 		S_AXIS_TREADY	=> s_axis_tready,
 		S_AXIS_TDATA	=> s_axis_tdata,
 		ready_enb => ready_enb,
+		instr_valid => instr_valid,
 		S_AXIS_TVALID	=> s_axis_tvalid
 	);
 
--- myaxistream_1_0/hdl/myaxistream_v1_0_S_AXIS.vhd
+++ myaxistream_1_0/hdl/myaxistream_v1_0_S_AXIS.vhd
@@ -3,35 +3,42 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity myaxistream_v1_0_S_AXIS is
-	generic (
-		C_S_AXIS_TDATA_WIDTH	: integer	:= 32
-	);
-	port (
-	    ready_enb : in std_logic;
-	    led : out std_logic;
-	    data_o : out std_logic_vector(2 downto 0);
-		S_AXIS_ACLK	: in std_logic;
-		S_AXIS_ARESETN	: in std_logic;
-		S_AXIS_TREADY	: out std_logic;
-		S_AXIS_TDATA	: in std_logic_vector(C_S_AXIS_TDATA_WIDTH-1 downto 0);
-		S_AXIS_TVALID	: in std_logic
-	);
+    generic (
+        C_S_AXIS_TDATA_WIDTH    : integer   := 64
+    );
+    port (
+        ready_enb : in std_logic;
+        instr_valid : out std_logic;
+        data_1_o : out std_logic_vector(31 downto 0);
+        data_2_o : out std_logic_vector(31 downto 0);
+        S_AXIS_ACLK : in std_logic;
+        S_AXIS_ARESETN  : in std_logic;
+        S_AXIS_TREADY   : out std_logic;
+        S_AXIS_TDATA    : in std_logic_vector(C_S_AXIS_TDATA_WIDTH-1 downto 0);
+        S_AXIS_TVALID   : in std_logic
+    );
 end myaxistream_v1_0_S_AXIS;
 
 architecture arch_imp of myaxistream_v1_0_S_AXIS is
-    component test_design is
-    Port (
-        data_i : in std_logic_vector(31 downto 0);
-        S_AXIS_ACLK : in std_logic;
-        S_AXIS_ARESETN : in std_logic;
-        led : out std_logic;
-        data_o : out std_logic_vector(2 downto 0)
-     );
-     end component;
+
     type FSM is (WAIT_S, TX1_S, TX2_S);
     signal state_t, next_state_t : FSM;
     signal tready : std_logic;
+    signal fsm_start_edge, fsm_start_prev : std_logic;
+
 begin 
+    -- Processo per rilevamento fronte di salita di fsm_start
+    process(S_AXIS_ACLK, S_AXIS_ARESETN)
+    begin
+        if S_AXIS_ARESETN = '0' then
+            fsm_start_prev  <= '0';
+            fsm_start_edge  <= '0';
+        elsif rising_edge(S_AXIS_ACLK) then
+            fsm_start_edge <= '1' when (ready_enb = '1' and fsm_start_prev = '0') else '0';
+            fsm_start_prev <= ready_enb;
+        end if;
+    end process;
+
     S_AXIS_TREADY <= tready;
     process(all) 
     begin
@@ -40,7 +47,7 @@ begin
         else 
             case state_t is
                 when WAIT_S => 
-                     if ready_enb = '1' then
+                     if fsm_start_edge = '1' then
                         next_state_t <= TX1_S;
                      else 
                         next_state_t <= WAIT_S;
@@ -73,28 +80,30 @@ begin
     process(S_AXIS_ACLK) 
     begin
            if rising_edge(S_AXIS_ACLK) then
-            if S_AXIS_ARESETN = '0' then
-                tready <= '0';
-            else 
-                case state_t is
-                    when WAIT_S =>
-                        tready <= '0';
-                    when TX1_S =>
-                        tready <= '1';
-                    when TX2_S => 
-                        tready <= '0';
-                    when others => 
-                        tready <= '0';
-                end case;
-           end if;
+               if S_AXIS_ARESETN = '0' then
+                    tready <= '0';
+                    instr_valid <= '0';
+                    data_1_o <= (others => '0');
+                    data_2_o <= (others => '0');
+                else 
+                    case state_t is
+                        when WAIT_S =>
+                            tready <= '0';
+                            instr_valid <= '0';
+                        when TX1_S =>
+                            tready <= '1';
+                            instr_valid <= '1';
+                        when TX2_S => 
+                            data_1_o <= S_AXIS_TDATA(31 downto 0);
+                            data_2_o <= S_AXIS_TDATA(63 downto 32);
+                            instr_valid <= '0';
+                            tready <= '0';
+                        when others => 
+                            tready <= '0';
+                            instr_valid <= '0';
+                    end case;
+               end if;
         end if;
     end process;
-    
-    U1 : test_design port map(
-        S_AXIS_ACLK => S_AXIS_ACLK,
-        led => led, 
-        S_AXIS_ARESETN => S_AXIS_ARESETN,
-        data_i => S_AXIS_TDATA,
-        data_o => data_o
-    );
+   
 end arch_imp;
