e44949be9df7ea60fdade33dee8ee9e3eb9cfbb9
ValerioCilento <valecilento02@gmail.com>
2026-08-12 15:45:21 +0200

    +utils/compile

---
+++ utils/compile/waves.tcl
@@ -0,0 +1,5 @@
+add wave -group "SCHEDULER" sim:/tb_spScheduler_CORE/SCHEDULER_INST/*
+add wave -group "SCHEDULER_FIFO" sim:/tb_spScheduler_CORE/SCHEDULER_INST/GEN_FIFO(0)/FIFO/*
+add wave -group "CORE" sim:/tb_spScheduler_CORE/CORE_INST/*
+add wave -group "PIPE" sim:/tb_spScheduler_CORE/CORE_INST/PIPE/*
+add wave -group "EXEC" sim:/tb_spScheduler_CORE/CORE_INST/EXEC/*
